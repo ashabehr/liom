a5c49ef91088cff2248ed866a5edaf895ac15cb8
sadeghpro <sadeghpro91@gmail.com>
2024-09-28 12:04:54 +0330

    add picker

--- src/pages/plasmic-host.tsx
+++ src/pages/plasmic-host.tsx
@@ -18,6 +18,10 @@ import { ApiRequest, apiRequestMeta } from "@/fragment/components/api-request";
 import { Slider, sliderMeta } from "@/fragment/components/slider";
 import { Chart, chartMeta } from "@/fragment/components/chart";
 import { Textarea, textareaMeta } from "@/fragment/components/textarea";
+import {DatePickers,DatePickersMeta} from "@/components/DatePickers";
+import {Pickers,PickersMeta} from "@/components/Picker";
+import {PullToRefresh,PullToRefreshMeta} from "@/components/PullToRefresh";
+
 
 export default function PlasmicHost() {
   return <PlasmicCanvasHost />;
@@ -36,3 +40,6 @@ registerComponent(Slider, sliderMeta);
 registerComponent(ApiRequest, apiRequestMeta);
 registerComponent(Chart, chartMeta);
 registerComponent(Textarea, textareaMeta);
+registerComponent(DatePickers, DatePickersMeta);
+registerComponent(Pickers, PickersMeta);
+registerComponent(PullToRefresh, PullToRefreshMeta);
