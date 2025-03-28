268c48ba79e7a684bbdb851b41605f142c3fcad7
ashabehr <ashabehrbakhshaee@yahoo.com>
2025-03-28 14:33:09 +0330

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -21,7 +21,7 @@ messaging.onBackgroundMessage((payload) => {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: "https://example.com/icon.png"
+    icon: "/icons/favicon.ico"
   };
 
   return self.registration.showNotification(notificationTitle, notificationOptions);
