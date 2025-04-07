8414f00476466a300d8e87492609ae8c4155103d
ashabehr <ashabehrbakhshaee@yahoo.com>
2025-04-07 15:26:39 +0400

    Update plasmic-host.tsx

--- src/pages/plasmic-host.tsx
+++ src/pages/plasmic-host.tsx
@@ -21,6 +21,8 @@ import { Textarea, textareaMeta } from "@/fragment/components/textarea";
 import {DatePickers,DatePickersMeta} from "@/components/DatePickers";
 import {Pickers,PickersMeta} from "@/components/Pickers";
 import {PullToRefresh,PullToRefreshMeta} from "@/components/PullToRefresh";
+import {BackButtonManager,BackButtonManagerMeta} from "@/components/BackButtonManager";
+
 
 
 export default function PlasmicHost() {
@@ -43,3 +45,4 @@ registerComponent(Textarea, textareaMeta);
 registerComponent(DatePickers, DatePickersMeta);
 registerComponent(Pickers, PickersMeta);
 registerComponent(PullToRefresh, PullToRefreshMeta);
+registerComponent(BackButtonManager, BackButtonManagerMeta);
