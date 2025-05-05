1e72ee1c14209334eda05e9dc06d0bbde31264f1
ashabehr <ashabehrbakhshaee@yahoo.com>
2025-05-05 12:09:35 +0400

    Update fragment.tsx

--- src/fragment/fragment.tsx
+++ src/fragment/fragment.tsx
@@ -62,6 +62,7 @@ export const Fragment = ({
               duration,
               position: placement,
             });
+         }
       },
       apiRequest: async (
         method: "GET" | "POST" | "DELETE" | "PUT" | "PATCH" = "GET",
