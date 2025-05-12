2fbd472ab3c934cf6b96378de4b2216ec9eb239b
ashabehr <ashabehrbakhshaee@yahoo.com>
2025-05-12 16:11:25 +0400

    Update fragment.tsx

--- src/fragment/fragment.tsx
+++ src/fragment/fragment.tsx
@@ -155,24 +155,24 @@ export const Fragment = ({
               break;
             }
             case "#skinCare": {
-              const link = `https://tools.liom.app/self-medication/?type=skinCare&inApp=${inApp}&token=${token}&selectStep=0&userId=${userId}&them=${theme}`;
+              const link = `https://tools.liom.app/self-medication/?type=skinCare&inApp=${inApp}&token=${token}&selectStep=0&userId=${userId}&theme=${theme}`;
               sendMessage("روتین پوستی", link,inWebViow);
               break;
             }
             case "#hair_care": {
-              const link = `https://tools.liom.app/self-medication/?type=hair_care&inApp=${inApp}&token=${token}&selectStep=0&userId=${userId}&them=${theme}`;
+              const link = `https://tools.liom.app/self-medication/?type=hair_care&inApp=${inApp}&token=${token}&selectStep=0&userId=${userId}&theme=${theme}`;
               sendMessage("روتین مراقبت از مو", link,inWebViow);
               break;
             }
             case "#adhd_treatment_sub": {
-              const link = `https://tools.liom.app/self-medication/?type=adhd_treatment_sub&inApp=${inApp}&token=${token}&selectStep=0&userId=${userId}&them=${theme}`;
+              const link = `https://tools.liom.app/self-medication/?type=adhd_treatment_sub&inApp=${inApp}&token=${token}&selectStep=0&userId=${userId}&theme=${theme}`;
               sendMessage("درمان اختلال ADHD", link,inWebViow);
               break;
             }
 
             case "#stretch_marks": {
 
-              const link = `https://tools.liom.app/self-medication/?type=stretch_marks&inApp=${inApp}&token=${token}&selectStep=0&userId=${userId}&them=${theme}`;
+              const link = `https://tools.liom.app/self-medication/?type=stretch_marks&inApp=${inApp}&token=${token}&selectStep=0&userId=${userId}&theme=${theme}`;
               sendMessage("ترک پوستی", link,inWebViow);
               break;
             }
