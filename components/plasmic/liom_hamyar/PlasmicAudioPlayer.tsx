7a6d334c0bb2d7a182070d4a60b44d94271d70b3
ashabehr <ashabehrbakhshaee@yahoo.com>
2025-08-16 13:10:30 +0330

    Update PlasmicAudioPlayer.tsx

--- components/plasmic/liom_hamyar/PlasmicAudioPlayer.tsx
+++ components/plasmic/liom_hamyar/PlasmicAudioPlayer.tsx
@@ -61,8 +61,6 @@ import {
 import { Embed } from "@plasmicpkgs/plasmic-basic-components";
 import { _useGlobalVariants } from "./plasmic"; // plasmic-import: suVPi77vb6vv9K5rYJwyxC/projectModule
 import { _useStyleTokens } from "./PlasmicStyleTokensProvider"; // plasmic-import: suVPi77vb6vv9K5rYJwyxC/styleTokensProvider
-import { _useStyleTokens as useStyleTokens_antd_5_hostless } from ""; // plasmic-import: ohDidvG9XsCeFumugENU3J/styleTokensProvider
-import { _useStyleTokens as useStyleTokens_plasmic_rich_components } from ""; // plasmic-import: jkU633o1Cz7HrJdwdxhVHk/styleTokensProvider
 
 import "@plasmicapp/react-web/lib/plasmic.css";
 
