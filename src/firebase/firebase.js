0d8fcbc0540329ab5b23fbaffb301994d05b6def
ashabehr <ashabehrbakhshaee@yahoo.com>
2025-08-25 10:13:33 +0330

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,18 +1,19 @@
+// firebase.ts
 import { initializeApp } from "firebase/app";
-import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBVtKyIzcD0xVEMOjeMYjDdNRozFVVrmRo",
   authDomain: "liom-31952.firebaseapp.com",
   databaseURL: "https://liom-31952.firebaseio.com",
   projectId: "liom-31952",
-  storageBucket: "liom-31952.firebasestorage.app",
+  storageBucket: "liom-31952.appspot.com",
   messagingSenderId: "518322220404",
   appId: "1:518322220404:web:09527c8a42f2f017d89021",
-  measurementId: "G-TVWYWYEH1D"
+  measurementId: "G-TVWYWYEH1D",
 };
 
 const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
 
-export { messaging, getToken, onMessage };
+export { messaging };
