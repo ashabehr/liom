572cc3c29bbe9dc75b0399b58868b16814278740
ashabehr <ashabehrbakhshaee@yahoo.com>
2025-09-03 09:30:59 +0330

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,9 @@ import Head from "next/head";
 import { useEffect, useState } from "react";
 import NotificationToast from "@/components/NotificationToast";
 
+import { NewViewContextProvider } from "../../components/plasmic/liom_hamyar/PlasmicGlobalVariant__NewView.tsx";
 export default function MyApp({ Component, pageProps }: AppProps) {
+    const [newView, setNewView] = useState<"newView" | undefined>(undefined);
   const [modalData, setModalData] = useState<{
     title: string;
     body: string;
@@ -15,6 +17,14 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   } | null>(null);
 
   useEffect(() => {
+    if (typeof window !== "undefined") {
+      const saved = localStorage.getItem("newView");
+      if (saved === "true") {
+        setNewView("newView"); // مقداردهی به Context
+      } else {
+        setNewView(undefined);
+      }
+    }
     if (typeof window !== "undefined" && "serviceWorker" in navigator) {
       navigator.serviceWorker.getRegistrations().then((registrations) => {
         const alreadyRegistered = registrations.some((reg) =>
@@ -62,18 +72,20 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/icons/favicon.ico" type="image/x-icon" />
         <link rel="manifest" href="/manifest.json" />
       </Head>
-      <PlasmicRootProvider Head={Head}>
-        <Component {...pageProps} />
 
-        {modalData && (
-          <NotificationToast
-            title={modalData.title}
-            body={modalData.body}
-            action={modalData.action}
-            icon="/icons/favicon.ico"
-            onClose={() => setModalData(null)}
-          />
-        )}
+      <PlasmicRootProvider Head={Head}>
+        <NewViewContextProvider value={newView}>
+          <Component {...pageProps} />
+          {modalData && (
+            <NotificationToast
+              title={modalData.title}
+              body={modalData.body}
+              action={modalData.action}
+              icon="/icons/favicon.ico"
+              onClose={() => setModalData(null)}
+            />
+          )}
+        </NewViewContextProvider>
       </PlasmicRootProvider>
     </>
   );
