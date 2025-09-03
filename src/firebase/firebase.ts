d22c74c5dac78f64850beeb98f6c9d68d45c01df
ashabehr <ashabehrbakhshaee@yahoo.com>
2025-09-03 09:49:02 +0330

    Update firebase.ts

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -7,12 +7,11 @@ const firebaseConfig = {
   authDomain: "liom-31952.firebaseapp.com",
   databaseURL: "https://liom-31952.firebaseio.com",
   projectId: "liom-31952",
-  storageBucket: "liom-31952.appspot.com",
+  storageBucket: "liom-31952.firebasestorage.app",
   messagingSenderId: "518322220404",
   appId: "1:518322220404:web:09527c8a42f2f017d89021",
-  measurementId: "G-TVWYWYEH1D",
+  measurementId: "G-TVWYWYEH1D"
 };
-
 // اطمینان از اینکه app فقط یک بار initialize شود
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
