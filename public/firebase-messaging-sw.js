cabdaa712253d747304a04456ec07541a039b520
ashabehr <ashabehrbakhshaee@yahoo.com>
2025-09-03 09:49:55 +0330

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -7,13 +7,12 @@ self.addEventListener('install', () => self.skipWaiting());
 importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compat.js');
 
-// 3) پیکربندی Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBVtKyIzcD0xVEMOjeMYjDdNRozFVVrmRo",
   authDomain: "liom-31952.firebaseapp.com",
   databaseURL: "https://liom-31952.firebaseio.com",
   projectId: "liom-31952",
-  storageBucket: "liom-31952.appspot.com",
+  storageBucket: "liom-31952.firebasestorage.app",
   messagingSenderId: "518322220404",
   appId: "1:518322220404:web:09527c8a42f2f017d89021",
   measurementId: "G-TVWYWYEH1D"
