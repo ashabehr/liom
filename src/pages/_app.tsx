82a16de36cd022100fb94e3a75db41d446f6e09d
ashabehr <ashabehrbakhshaee@yahoo.com>
2025-09-04 20:52:59 +0330

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,57 +16,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     action?: string | null;
   } | null>(null);
 
-  // گرفتن کوکی
-// گرفتن کوکی و برگرداندن اولین آیتم از JSON
-function getCookie(name: string): any {
-  const cookies = document.cookie.split("; ");
-  for (let cookie of cookies) {
-    const [key, value] = cookie.split("=");
-    if (key === name) {
-      try {
-        return JSON.parse(value)[0]; // فقط آیتم اول
-      } catch (err) {
-        console.error("❌ خطا در پارس کوکی:", err);
-        return null;
-      }
-    }
-  }
-  return "";
-}
-
-    
-function sendTokenToServer(token: string, retryCount = 0) {
-  const savedToken = window.localStorage.getItem("fcmToken");
-  if (savedToken === token) return;
-
-  const authToken = getCookie("token");
-  if (!authToken) return;
-
-  window.localStorage.setItem("fcmToken", token);
-
-  fetch("https://n8n.staas.ir/webhook/rest/user/setFcm", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: authToken
-    },
-    body: JSON.stringify({ fcm: token })
-  })
-    .then((res) => {
-      if (!res.ok) throw new Error("خطا در ارسال توکن");
-      return res.json();
-    })
-    .then(() => console.log("✅ FCM token ثبت شد"))
-    .catch((err) => {
-      if (retryCount < 3) {
-        const delay = (retryCount + 1) * 2000; // ۲s, ۴s, ۶s
-        console.warn(`❌ خطا در ارسال توکن، تلاش مجدد در ${delay / 1000} ثانیه...`);
-        setTimeout(() => sendTokenToServer(token, retryCount + 1), delay);
-      } else {
-        console.error("❌ ارسال توکن بعد از چند بار تلاش ناموفق بود:", err);
-      }
-    });
-}
   useEffect(() => {
 
     if ("serviceWorker" in navigator) {
@@ -105,12 +54,11 @@ function sendTokenToServer(token: string, retryCount = 0) {
 
       import("../firebase/fcm").then(
         ({ requestPermission, onMessageListener }) => {
-          requestPermission().then((token) => {
-            if (token) {
-              sendTokenToServer(token);
-              console.log("✅ ذخیره شد FCM Token:", token);
-            }
-          });
+           requestPermission().then((token) => {
+              if (token) {
+                console.log("✅ FCM Token:", token);
+              }
+            });
 
           onMessageListener((payload) => {
             console.log("📩 پیام Foreground -app:", payload);
