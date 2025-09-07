a9530a72348fea518e3f678eeefb97f6d3f1a6f7
ashabehr <ashabehrbakhshaee@yahoo.com>
2025-09-07 09:55:19 +0330

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -13,3 +13,53 @@ firebase.initializeApp({
 });
 
 firebase.messaging();
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close(); // بستن نوتیف
+  event.preventDefault();
+
+  const action = event.notification.data?.action;
+  let targetUrl = 'https://apps.liom.app/login'; // پیش فرض
+
+  if (action) {
+    const pureAction = action.replace('#', '').split('-')[0];
+    const actionParam = action.split('-')[1];
+
+    switch (pureAction) {
+      case 'healthSubscription':
+        targetUrl = 'https://apps.liom.app/shop';
+        break;
+      case 'calendar':
+        targetUrl = 'https://apps.liom.app/main';
+        break;
+      case 'main':
+        targetUrl = 'https://apps.liom.app/main';
+        break;
+      case 'hamyar':
+        targetUrl = 'https://apps.liom.app/hamyar-add';
+        break;
+      case 'maincalendar':
+        targetUrl = 'https://apps.liom.app/main';
+        break;
+      case 'specialOffer':
+        targetUrl = 'https://apps.liom.app/offers/special';
+        break;
+      case 'orderStatus':
+        targetUrl = `https://apps.liom.app/orders/status/${actionParam}`;
+        break;
+      case 'newFeature':
+        targetUrl = 'https://apps.liom.app/features/new';
+        break;
+      case 'post':
+        targetUrl = `https://old.liom.app/social/?post=${actionParam}`;
+        break;
+      default:
+        targetUrl = 'https://apps.liom.app/login';
+        break;
+    }
+  }
+
+  event.waitUntil(
+    clients.openWindow(targetUrl)
+  );
+});
