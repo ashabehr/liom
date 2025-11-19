f6505a489fd2c245f824f073607709d06d4026d1
ashabehr <ashabehrbakhshaee@yahoo.com>
2025-11-19 12:45:45 +0330

    Update fragment.tsx

--- src/fragment/fragment.tsx
+++ src/fragment/fragment.tsx
@@ -383,7 +383,11 @@ export const Fragment = ({
 
             case "#healthSubscription": {
               if (typeof window !== "undefined" && window.FlutterChannel && typeof window.FlutterChannel.postMessage === "function") {
-                window.FlutterChannel.postMessage(action);}
+                window.FlutterChannel.postMessage(action
+             else {    
+                const link = `/shop/?token=${token}`;
+                sendMessage("بیوریتم", link,inWebViow);
+              }
               break;
             }
               
